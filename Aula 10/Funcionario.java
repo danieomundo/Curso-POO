16357c4990ed711f6c071853e86feb2c312b2c67
Dani <daniele.ddsp@gmail.com>
2022-11-06 03:35:47 -0300

    subclasse funcionario

---
+++ Aula 10/Funcionario.java
@@ -0,0 +1,26 @@
+package aula10;
+
+public class Funcionario extends Pessoa {
+    
+    private String setor;
+    private boolean trabalhando;    
+    
+    public void setSetor(String set){
+        this.setor = set;
+    }
+    
+    public String getSetor(){
+        return this.setor;
+    }
+    
+    public void setTrabalhando(boolean tr){
+        this.trabalhando = tr;
+    }
+    
+    public boolean getTrabalhando(){
+        return this.trabalhando;
+    }
+    
+    public void mudarTrabalho(){}
+    
+}
