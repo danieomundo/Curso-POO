dbacd365ab61ffc47c44b38adb0cbf19e00cde8a
Dani <daniele.ddsp@gmail.com>
2022-11-06 12:34:55 -0300

    visitante

---
+++ ProjetoPessoas/Visitante.java
@@ -0,0 +1,8 @@
+package ProjetoPessoas;
+
+public class Visitante extends Pessoa { // Visitante é especialização de Pessoa.
+    
+// Classe-Exemplo de Herança Pobre.   
+// Ela apenas herda os atributos e métodos da Superclasse Pessoa, sem implementar mais nenhum.
+    
+}
