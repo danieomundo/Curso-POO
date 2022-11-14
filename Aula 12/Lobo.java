e501938227a8766248822dea0e4a81a940d1985a
Dani <daniele.ddsp@gmail.com>
2022-11-14 09:34:57 -0300

    lobo

---
+++ Aula 12/Lobo.java
@@ -0,0 +1,19 @@
+package aula12e13;
+public class Lobo extends Mamifero {
+
+    @Override
+    public void locomover(){
+        System.out.println("Andando");
+    }
+      
+    @Override
+    public void alimentar(){
+        System.out.println("Comendo");
+    }
+    
+    @Override
+    public void emitirSom(){
+        System.out.println("Auuuuuuuuuu");
+    }
+          
+}
