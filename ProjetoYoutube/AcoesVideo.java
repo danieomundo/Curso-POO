cdc76ad97e2aa793eda83cccfb0fe130330da8fa
Dani <daniele.ddsp@gmail.com>
2022-11-14 09:36:13 -0300

    interface

---
+++ ProjetoYoutube/AcoesVideo.java
@@ -0,0 +1,11 @@
+package projetoyoutube;
+
+public interface AcoesVideo {
+
+    // somente assinaturas de métodos abstratos, sem implementação!
+    
+    public abstract void play();   
+    public abstract void pause();   
+    public abstract void like();
+       
+}
